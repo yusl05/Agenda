41c04056c5d64548b602c98dcffd4791f689973f
yusl05 <jesed2005ma@gmail.com>
2025-09-08 23:25:32 -0600

    Se agregaron clases

---
+++ Agenda/Utilidades/BaseDatosJson.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agenda.Utilidades
+{
+    public class BaseDatosJson
+    {
+        public List<Persona> personas { get; set; }
+
+        public DateTime actualizacion { get; set; }
+
+        public int totalRegistros { get; set; }
+
+        public BaseDatosJson() {
+            personas = new List<Persona>();
+            actualizacion = DateTime.Now; 
+        }  
+
+    }
+}
---
+++ Agenda/Utilidades/Persona.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agenda.Utilidades
+{
+    public class Persona
+    {
+        public String nombre { get; set; }
+        public String apPat { get; set; }
+        public String apMat { get; set; }
+        public String direccion { get; set; }
+        public String telefono { get; set; }
+        public String correo { get; set; }
+    }
+}
