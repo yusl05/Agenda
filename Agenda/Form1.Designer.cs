532914f7a81b5099151c56cb373616d4eeb1d3a0
yusl05 <jesed2005ma@gmail.com>
2025-09-09 23:03:30 -0600

    Se agregaron funciones

--- Agenda/Form1.Designer.cs
+++ Agenda/Form1.Designer.cs
@@ -29,12 +29,157 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.dGVDatos = new System.Windows.Forms.DataGridView();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.tmrTiempo = new System.Windows.Forms.Timer(this.components);
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dGVDatos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.dGVDatos);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.btnEliminar);
+            this.splitContainer1.Panel2.Controls.Add(this.statusStrip1);
+            this.splitContainer1.Size = new System.Drawing.Size(1309, 443);
+            this.splitContainer1.SplitterDistance = 347;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.statusStrip1.Location = new System.Drawing.Point(0, 70);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(1309, 22);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // dGVDatos
+            // 
+            this.dGVDatos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dGVDatos.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6});
+            this.dGVDatos.Location = new System.Drawing.Point(0, 0);
+            this.dGVDatos.Name = "dGVDatos";
+            this.dGVDatos.RowHeadersWidth = 62;
+            this.dGVDatos.RowTemplate.Height = 28;
+            this.dGVDatos.Size = new System.Drawing.Size(1309, 344);
+            this.dGVDatos.TabIndex = 0;
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(27, 18);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(98, 39);
+            this.btnEliminar.TabIndex = 1;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            // 
+            // tmrTiempo
+            // 
+            this.tmrTiempo.Enabled = true;
+            this.tmrTiempo.Interval = 1000;
+            this.tmrTiempo.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Nombre";
+            this.Column1.MinimumWidth = 8;
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 200;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Apellido paterno";
+            this.Column2.MinimumWidth = 8;
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 150;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Apellido materno";
+            this.Column3.MinimumWidth = 8;
+            this.Column3.Name = "Column3";
+            this.Column3.Width = 150;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Dirección";
+            this.Column4.MinimumWidth = 8;
+            this.Column4.Name = "Column4";
+            this.Column4.Width = 250;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Teléfono";
+            this.Column5.MinimumWidth = 8;
+            this.Column5.Name = "Column5";
+            this.Column5.Width = 200;
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "Correo";
+            this.Column6.MinimumWidth = 8;
+            this.Column6.Name = "Column6";
+            this.Column6.Width = 200;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1309, 443);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dGVDatos)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.DataGridView dGVDatos;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Timer tmrTiempo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
     }
 }
 
--- Agenda/Form1.cs
+++ Agenda/Form1.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using Agenda.Utilidades;
+using Newtonsoft.Json;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +16,69 @@ namespace Agenda
 {
     public partial class Form1 : Form
     {
+        public string rutaJson = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "datos.json");
+        //string contenido = File.ReadAllText(rutaJson);
+
         public Form1()
         {
+            if (!File.Exists(rutaJson))
+            {
+                var datosIniciales = new BaseDatosJson
+                {
+                    personas = new List<Persona>(),
+                    totalRegistros = 0
+                };
+
+                guardarJson(datosIniciales);
+            }
+
             InitializeComponent();
+            tmrTiempo.Interval = 5000;
+            tmrTiempo.Tick += timer1_Tick;
+            tmrTiempo.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            var datos = CargarDatos();
+            guardarJson(datos);
+        }
+
+        private BaseDatosJson CargarDatos()
+        {
+            var registros = new BaseDatosJson();
+            foreach (DataGridViewRow fila in dGVDatos.Rows)
+            {
+                if (fila.IsNewRow) continue;
+                var persona = new Persona();
+                {
+                    persona.nombre = fila.Cells[0].Value?.ToString() ?? "";
+                    persona.apPat = fila.Cells[1].Value?.ToString() ?? "";
+                    persona.apMat = fila.Cells[2].Value?.ToString() ?? "";
+                    persona.direccion = fila.Cells[3].Value?.ToString() ?? "";
+                    persona.telefono = fila.Cells[4].Value?.ToString() ?? "";
+                    persona.correo = fila.Cells[5].Value?.ToString() ?? "";
+                }
+                registros.personas.Add(persona);
+
+            }
+            registros.totalRegistros = registros.personas.Count;
+            return registros;
+
+        }
+
+        public void guardarJson(BaseDatosJson lista)
+        {
+            var caracteristicas = new JsonSerializerSettings
+            {
+                Formatting = Formatting.Indented,
+                DateFormatHandling = DateFormatHandling.IsoDateFormat,
+                NullValueHandling = NullValueHandling.Ignore,
+
+            };
+
+            string json = JsonConvert.SerializeObject(lista, caracteristicas);
+            File.WriteAllText(rutaJson, json);
         }
     }
 }
